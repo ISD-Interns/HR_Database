5515abe6b8860c6a88d1c358bb42372d4e8332b7
Liam Schroth <schrothl@sonoma.edu>
2021-04-15 15:11:51 -0700

    add identity, EditionName -> Name

--- HR_Database/dbo/Tables/HR_editions.sql
+++ HR_Database/dbo/Tables/HR_editions.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[HR_editions] (
-    [Id]          INT           NOT NULL,
-    [PlatformId]  INT           NOT NULL,
-    [GameId]      INT           NOT NULL,
-    [EditionName] NVARCHAR (50) NOT NULL,
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [PlatformId] INT           NOT NULL,
+    [GameId]     INT           NOT NULL,
+    [Name]       NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_HR_editions] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
