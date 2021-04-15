fc845749776ba9d863b33d80acd59f8351d1b499
Liam Schroth <schrothl@sonoma.edu>
2021-04-15 15:12:13 -0700

    FavoriteGameId

--- HR_Database/dbo/Tables/HR_users.sql
+++ HR_Database/dbo/Tables/HR_users.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[HR_users] (
-    [username]    NVARCHAR (50) NOT NULL,
-    [Region]      NVARCHAR (50) NULL,
-    [Twitch]      NVARCHAR (50) NULL,
-    [TotalPoints] INT           CONSTRAINT [DF_HR_users_TotalPoints] DEFAULT ((0)) NOT NULL,
-    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [username]       NVARCHAR (50) NOT NULL,
+    [Region]         NVARCHAR (50) NULL,
+    [Twitch]         NVARCHAR (50) NULL,
+    [TotalPoints]    INT           CONSTRAINT [DF_HR_users_TotalPoints] DEFAULT ((0)) NOT NULL,
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [FavoriteGameId] INT           NOT NULL,
     CONSTRAINT [PK_HR_users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
